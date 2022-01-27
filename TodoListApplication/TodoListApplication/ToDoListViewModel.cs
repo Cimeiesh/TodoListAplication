248986535eede5eef83f10e0c0e723f64511fa9c
Sanna Edström <70255968+Cimeiesh@users.noreply.github.com>
2022-01-27 11:34:05 +0100

    The Application

---
+++ TodoListApplication/TodoListApplication/ToDoListViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using ToDoListApp;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TodoListApplication
+{
+    public class ToDoListViewModel
+    {
+        public ObservableCollection<TodoItems> TodoItem { get; set; }
+        public string NewToDo { get; set; }
+
+        
+        
+
+        public ToDoListViewModel()
+        {
+            TodoItem = new ObservableCollection<TodoItems>();
+
+            TodoItem.Add(new TodoItems("Shopping The Book", false));
+            TodoItem.Add(new TodoItems("Cook Dinner", false));
+            TodoItem.Add(new TodoItems("Reading Time", false));
+            TodoItem.Add(new TodoItems("Cleaning", false));
+            TodoItem.Add(new TodoItems("Walk the Dog", false));
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        void AddTodoItem()
+        {
+            TodoItem.Add(new TodoItems(NewToDo, false));
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        void RemoveTodoItem(object o)
+        {
+            TodoItems todoItemBeingRemoved = o as TodoItems;
+            TodoItem.Remove(todoItemBeingRemoved);
+        }
+    }
+}
---
+++ TodoListApplication/TodoListApplication/TodoItems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListApp
+{
+    public class TodoItems
+    {
+        public string TodoText { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public TodoItems(string TodoText, bool IsCompleted)
+        {
+            this.TodoText = TodoText;
+            this.IsCompleted = IsCompleted;
+        }
+    }
+}
